45b0993d8de7f0a92798d06cf5f57955973ced22
talk2dennis <adigwedennis@yahoo.com>
2024-06-25 12:30:44 +0100

    completed task 2: using .then catch and finally

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+function handleResponseFromAPI(promise) {
+  const response = { status: 200, body: 'success' };
+  return promise
+    .then(() => response)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
