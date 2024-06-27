26bd5e9254d57b7ae0f040169a5e79beb10160f4
talk2dennis <adigwedennis@yahoo.com>
2024-06-27 21:09:24 +0100

    cloned without the fields

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,8 +1,8 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') {
-      throw new Error('brand, motor and color must be of type string');
-    }
+    // if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') {
+    //   throw new Error('brand, motor and color must be of type string');
+    // }
 
     this._brand = brand;
     this._motor = motor;
@@ -10,6 +10,6 @@ export default class Car {
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    return new this.constructor();
   }
 }
