41c800013495b1863395d870457c5336eb1333a8
talk2dennis <adigwedennis@yahoo.com>
2024-06-28 16:40:38 +0000

    updated the buildingclass to behave like abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -4,6 +4,9 @@ export default class Building {
       throw new Error('Invalid argument type');
     }
     this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
@@ -15,8 +18,5 @@ export default class Building {
       throw new Error('Sqft must be a number');
     }
     this._sqft = value;
-  }
-  if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
