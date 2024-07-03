3266075b0904d74d3521e086c849494f73445068
talk2dennis <adigwedennis@yahoo.com>
2024-07-03 20:12:21 +0100

    added task 9 implemented namespace, interface and class inheritance

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+    export class Subject {
+        teacher: Subjects.Teacher;
+        
+        setTeacher(teacher: Subjects.Teacher) {
+            this.teacher = teacher;
+        }
+    }
+}
