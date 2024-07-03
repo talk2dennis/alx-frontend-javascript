43ffbca1a4c4e0616ec1c7daa8840c72991d1c42
talk2dennis <adigwedennis@yahoo.com>
2024-07-03 18:31:28 +0100

    completed isDirector and executeWorker functions

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -42,3 +42,12 @@ function createEmployee (salary: number | string): Director | Teacher {
   if (typeof salary === 'number' && salary < 500) return new Teacher();
   return new Director();
 }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) return employee.workDirectorTasks();
+  return employee.workTeacherTasks();
+}
