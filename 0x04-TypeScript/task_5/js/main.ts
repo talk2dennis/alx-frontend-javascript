9c645f8cdbb83c9d56a6718d600af1e08869df47
talk2dennis <adigwedennis@yahoo.com>
2024-07-03 20:58:44 +0100

    created major and minor functin to calculate the credits of students

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -7,3 +7,11 @@ interface MinorCredits {
     brand: 'minor';
     credits: number;
 }
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+    return subject1.credits + subject2.credits;
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
+    return subject1.credits + subject2.credits;
+}
